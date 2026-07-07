17d75a53fece141fe2eb702fb983d4ba1e657e38
acyubahiroo <a.cyubahiro@alustudent.com>
2026-07-07 10:10:36 +0200

    removed unecessary files

--- 0-print_list_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Print all integers of a list, one per line.
-
-    Args:
-        my_list: list of integers
-    """
-    for number in my_list:
-        print("{:d}".format(number))
